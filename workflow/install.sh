cc8932d01df16c37224fcf5a70f8e90bac5bf1ec
François-Xavier Lyonnet du Moutier <fx.du.moutier@gmail.com>
2021-09-28 17:50:58 +0200

    no more install script needed

--- workflow/install.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-cd scripts
-git clone https://github.com/CiTCoM-Lab/QuShape.git
-git clone https://github.com/afafbioinfo/NormProbing.git
-git clone https://github.com/afafbioinfo/IPANEMAP.git
