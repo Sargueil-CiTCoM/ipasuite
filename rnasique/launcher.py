ce0e5c828ac8e63255d1c796fffd4b8ce35b4c43
NaaaaaaaaN <pnlld529@gmail.com>
2024-04-15 21:06:24 +0200

    samples.tsv --> magnesium_condition

--- rnasique/launcher.py
+++ rnasique/launcher.py
@@ -182,7 +182,8 @@ class Launcher(object):
                     row['temperature'] = list_info['temperature'][1:]
                     if file.split('.')[-1] == 'txt' and list_info['probe_control_flag_unique_experiment_id'][0] == 'B':
                         replicate = file.split('_')[1][1:]
-                        control_file = [file_name for file_name in files if len(file_name.split('_')) >1 and replicate == file_name.split('_')[1][1:] and file_name != file][0]
+                        control_file = [file_name for file_name in files if len(file_name.split('_')) >1 and replicate == file_name.split('_')[1][1:] and \
+                                        list_info['magnesium'] == file_name.split('_')[3] and file_name != file][0]
                         row['probe_file'] = file
                         row['control_file'] = control_file
                         row['replicate'] = replicate
